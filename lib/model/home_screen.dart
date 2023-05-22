5f4605a116affbba31495169a5f16427c31afe65
Andreea <andelenasamur@gmail.com>
2023-05-22 13:41:25 +0200

    geolocator setting

--- lib/model/home_screen.dart
+++ lib/model/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app_flutter/controller/global_controller.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +10,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // call
+  final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold();
