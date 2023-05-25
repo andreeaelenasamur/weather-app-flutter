4cbcde49071541cccde249e21622914ddf649cde
Andreea <andelenasamur@gmail.com>
2023-05-25 10:36:39 +0200

    weather data - UI design

--- lib/model/weather_data_daily.dart
+++ lib/model/weather_data_daily.dart
@@ -4,6 +4,7 @@ class DataWeather {
   final int? tempMax;
   final String? weatherIcon;
   final int? dt;
+  final String dt_txt;
 
   DataWeather({
     required this.temperature,
@@ -11,6 +12,7 @@ class DataWeather {
     required this.tempMin,
     required this.weatherIcon,
     required this.dt,
+    required this.dt_txt,
   });
 }
 
@@ -24,23 +26,28 @@ class WeatherDataDaily {
       json['list'] as List<dynamic>,
     );
 
-    final List<DataWeather> weatherList = weatherDataList.map((weatherJson) {
-      final main = weatherJson['main'] as Map<String, dynamic>;
-      final dt = weatherJson['dt'] as int?;
-      final weather = weatherJson['weather'][0] as Map<String, dynamic>;
-      final temperature = (main['temp'] as num?)?.round();
-      final tempMin = (main['temp_min'] as num?)?.round();
-      final tempMax = (main['temp_max'] as num?)?.round();
-      final weatherIcon = weather['icon'] as String;
+    final List<DataWeather> weatherList = weatherDataList
+        .map((weatherJson) {
+          final main = weatherJson['main'] as Map<String, dynamic>;
+          final dt = weatherJson['dt'] as int?;
+          final weather = weatherJson['weather'][0] as Map<String, dynamic>;
+          final temperature = (main['temp'] as num?)?.round();
+          final tempMin = (main['temp_min'] as num?)?.round();
+          final tempMax = (main['temp_max'] as num?)?.round();
+          final weatherIcon = weather['icon'] as String;
+          final dt_txt = weatherJson['dt_txt'] as String;
 
-      return DataWeather(
-        temperature: temperature,
-        tempMax: tempMax,
-        tempMin: tempMin,
-        weatherIcon: weatherIcon,
-        dt: dt,
-      );
-    }).toList();
+          return DataWeather(
+            temperature: temperature,
+            tempMax: tempMax,
+            tempMin: tempMin,
+            weatherIcon: weatherIcon,
+            dt: dt,
+            dt_txt: dt_txt,
+          );
+        })
+        .where((element) => DateTime.parse(element.dt_txt).hour == 12)
+        .toList();
 
     return WeatherDataDaily(weatherList: weatherList);
   }
--- lib/widgets/daily_data_forecast.dart
+++ lib/widgets/daily_data_forecast.dart
@@ -1,13 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_app_flutter/model/weather_data_daily.dart';
+import 'package:weather_app_flutter/utils/custom_colors.dart';
 
 class DailyDataForecast extends StatelessWidget {
   final WeatherDataDaily weatherDataDaily;
   const DailyDataForecast({Key? key, required this.weatherDataDaily})
       : super(key: key);
 
+  // string manipulation
+  String getDay(final day) {
+    DateTime time = DateTime.fromMillisecondsSinceEpoch(day * 1000);
+    final x = DateFormat('EEE').format(time);
+    return x;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 400,
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: CustomColors.dividerLine.withAlpha(150),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            margin: const EdgeInsets.only(bottom: 10),
+            child: const Text("Next days",
+                style: TextStyle(
+                    color: CustomColors.textColorBlack, fontSize: 17)),
+          ),
+          dailyList(),
+        ],
+      ),
+    );
+  }
+
+  Widget dailyList() {
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: weatherDataDaily.weatherList.length > 7
+            ? 7
+            : weatherDataDaily.weatherList.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              Container(
+                  height: 60,
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        width: 80,
+                        child: Text(
+                          getDay(weatherDataDaily.weatherList[index].dt),
+                          style: const TextStyle(
+                              color: CustomColors.textColorBlack, fontSize: 13),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Image.asset(
+                            "assets/weather/${weatherDataDaily.weatherList[index].weatherIcon}.png"),
+                      ),
+                      Text(
+                          "${weatherDataDaily.weatherList[index].tempMax}º/${weatherDataDaily.weatherList[index].tempMin}")
+                    ],
+                  )),
+              Container(
+                height: 1,
+                color: CustomColors.dividerLine,
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
 }
